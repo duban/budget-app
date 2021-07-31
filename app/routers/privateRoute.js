45972d8d4bbfb3ed22621c53e324089505d4445a
duban <mikael.duban@gmail.com>
2021-07-31 16:45:37 +0800

    fix route path api

--- app/routers/privateRoute.js
+++ app/routers/privateRoute.js
@@ -2,10 +2,18 @@ const BudgetContoller = require('../controllers/BudgetController');
 module.exports = (app) => {
     app.route('/api/index')
         .get(BudgetContoller().index_home);
+    app.route('/app/budget')
+        .get(BudgetContoller().render_index);
     app.route('/api/budgets')
         .get(BudgetContoller().list_budget);
     app.route('/api/budget')
         .post(BudgetContoller().add_budget);
+    app.route('/api/expence')
+        .post(BudgetContoller().add_expense);
+    app.route('/api/expence/:id')
+        .put(BudgetContoller().edit_expense);
+    app.route('/api/expence/:id')
+        .delete(BudgetContoller().delete_expense);
     app.route('/api/budget/:id')
         .put(BudgetContoller().edit_budget);
     app.route('/api/budget/:id')
